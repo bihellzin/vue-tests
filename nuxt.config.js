2d2a9f2aa0285bbd271d493a9477285bb143b73c
bielzin <oliva.gabriel@outlook.com>
2022-12-15 22:50:30 -0300

    setup miragejs

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ export default {
   css: ['@/assets/css/main.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: ['@/plugins/miragejs'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: false,
@@ -57,4 +57,4 @@ export default {
       },
     },
   },
-}
+};
