3f817c0692a5daee629bd71ad5b8e5d62cd66bfc
bielzin <oliva.gabriel@outlook.com>
2022-12-19 23:10:06 -0300

    adds new tests and @types/jest

--- components/ProductCard.unit.spec.js
+++ components/ProductCard.unit.spec.js
@@ -1,13 +1,45 @@
 import { mount } from '@vue/test-utils';
 import ProductCard from '@/components/ProductCard';
+import { makeServer } from '@/miragejs/server';
 
 describe('Product Card - unit', () => {
+  let server;
+
+  beforeEach(() => {
+    server = makeServer({ environtment: 'test' });
+  });
+
+  afterEach(() => {
+    server.shutdown();
+  });
+
+  it('should match snapshot', () => {
+    const wrapper = mount(ProductCard, {
+      propsData: {
+        product: server.create('product', {
+          title: 'Relogio novo',
+          price: '45.00',
+          image:
+            'https://images.unsplash.com/photo-1495857000853-fe46c8aefc30?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
+        }),
+      },
+    });
+
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
   it('should mount', () => {
     const wrapper = mount(ProductCard, {
       propsData: {
-        product: {},
+        product: server.create('product', {
+          title: 'Relogio novo',
+          price: '45.00',
+        }),
       },
     });
+
     expect(wrapper.vm).toBeDefined();
+    expect(wrapper.text()).toContain('Relogio novo');
+    expect(wrapper.text()).toContain('45.00');
   });
 });
